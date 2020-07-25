8d3352d731db94df8f72f4f7f0a09ff54eb04ba0
Dennis Hummel <dhummel@gmail.com>
2020-07-25 18:52:46 +0300

    Added a drop shadow to the splash form

--- OpenDental/Forms/FormSplash.cs
+++ OpenDental/Forms/FormSplash.cs
@@ -13,5 +13,17 @@ namespace Imedisoft.Forms
             Size = new Size(500, 300);
             StartPosition = FormStartPosition.CenterScreen;
         }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x20000;
+
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
     }
 }
